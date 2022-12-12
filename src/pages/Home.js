55e2c9f67afb5b72723a5b4b223fecddd8034ed2
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2022-12-12 16:59:08 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 import Homepage from '../images/wallpapers/seminar3.jpg';
 
 import './Home.css';
@@ -5,7 +7,17 @@ import './Home.css';
 const Home = () => {
   return (
     <div className='home-container'>
-      <img src={Homepage} alt="" className="home-img" style={{ width:"100%", height:"100%" }}/>
+      <div className="bg">
+        <img src={Homepage} alt="" className="home-img" style={{ width:"100vw", height: "100vh"  }}/>
+      </div>
+      <div className="home-title">
+        <h1>SY 2022 - 2023</h1>
+      </div>
+      <div className='home-content'>
+        <h1>Seminars and Fieldtrips</h1>
+        <p>Josh Ivan C. Lanuevo</p>
+        <NavLink to="/about" className="home-btn">About</NavLink>
+      </div>
     </div>
   )
 }
