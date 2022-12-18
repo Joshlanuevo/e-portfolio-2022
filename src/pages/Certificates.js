415b176297c299cd61b483daeca6e31a7217d388
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2022-12-18 22:48:29 +0800

    Update Certificates.js

--- src/pages/Certificates.js
+++ src/pages/Certificates.js
@@ -1,156 +1,212 @@
-import BestPractices from '../images/seminars/Best Practices in IT cover.jpg';
-import DataMining from '../images/seminars/Data Mining cover.jpg';
-import DataPrivacy from '../images/seminars/Data Privacy cover.jpg';
-import CloudComputing from '../images/seminars/Fundamentals of Cloud Conmputing cover.png'
-import IPModular from '../images/seminars/IP Modular cover.png';
-import ITandCPECareers from '../images/seminars/IT and CPE Careers cover.jpg';
-import EmbeddedSystems from '../images/seminars/Latest Technology in Embedded cover.png';
-import LawsAndProfessional from '../images/seminars/Laws and Professional cover.png';
-import ReactNative from '../images/seminars/Mobile ui react native cover.jpg';
-import QATesting from '../images/seminars/QA testing cover.jpg';
-import WirelessNetworks from '../images/seminars/Wireless Networks and Communications Cover.png';
-import ArduinoProjects from '../images/seminars/Workshop Arduino cover.png';
+import ModalImage from "react-modal-image";
+
+import AdvancedProgCert from '../images/certs/ADVANCED PROGRAMMING & WEB TECHNOLOGY.jpg';
+import BackendWebDevCert from '../images/certs/BACKEND WEB DEVELOPMENT USING NODE JS, EXPRESS, and MongoDB-1.png';
+import BestPracticesCert from '../images/certs/BEST PRACTICES IN IT INDUSTRY.png';
+import DataMiningCert from '../images/certs/Data Mining & Data Analysis Certificate of Participation-1.png';
+import DataPrivacyCert from '../images/certs/DATA PRIVACY-1.png';
+import CloudComputingCert from '../images/certs/FUNDAMENTALS OF CLOUD COMPUTING AND EFFICIENT SOLUTIONS FOR CLOUD DATA STORAGE-1.png';
+import IPModularCert from '../images/certs/IP Modular Course-1.png';
+import ITandCPECareersCert from '../images/certs/IT AND CPE CAREERS IN THE IT INDUSTRY.jpg';
+import EmbeddedSystemsCert from '../images/certs/Latest Technology in Embedded Systems-1.png';
+import LawsAndProfessionalCert from '../images/certs/LAWS & PROFESSIONAL CONDUCTS FOR ICT PROFESSIONALS.jpg';
+import MixedAugmentedCert from '../images/certs/MIXED AUGMENTED AND VIRTUAL REALITY WEBINAR-1.png';
+import ReactNativeCert from '../images/certs/Mobile Application UI Development using React Native-1.png';
+import QATestingCert from '../images/certs/QA Testing Conference.png';
+import WirelessNetworkCert from '../images/certs/WIRELESS NETWORKS AND COMMUNICATIONS-1.png';
+import ArduinoProjectsCert from '../images/certs/WORKSHOP IN ARDUINO PROJECT-1.png';
 
 import './Certificates.css';
 
 const Certificates = () => {
   return (
-    <div className="container-cert">
-      <div className="cert-header">
+    <div className="certs-container">
+      <div className="certs-header">
         <h1>All Certificates</h1>
         <div className="underline"></div>
       </div>
-      <div className="container-content-cert">
+      <div className="total">
+        <h2>Total: 15</h2>
+      </div>
+      <div className="card-certs-container">
 
-        <div className="card">
-          <div className="card-header">
-            <img src={BestPractices} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>BEST PRACTICES IN IT INDUSTRY</h1>
-              <p>Learnings: Best practices in its sector are defined by leading technology research company Gartner as: A collection of tasks that maximizes the efficiency (cost and risk) or effectiveness (service level) of the business discipline or process to which it contributes. It must be transferrable, repeatable, and flexible across sectors.</p>
-              <button>View Certificate</button>
+        <div className="card-certs">
+          <ModalImage 
+            className='cert'
+            small={AdvancedProgCert}
+            large={AdvancedProgCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
 
-        <div className="card">
-          <div className="card-header">
-            <img src={DataMining} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>DATA MINING & DATA ANALYTICS</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">
-            <img src={DataPrivacy} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>DATA PRIVACY</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
+        <div className="card-certs">
+          <ModalImage 
+            className='cert'
+            small={BackendWebDevCert}
+            large={BackendWebDevCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
 
-        <div className="card">
-          <div className="card-header">
-            <img src={CloudComputing} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>FUNDAMENTALS OF CLOUD COMPUTING AND EFFICIENT SOLUTIONS FOR CLOUD DATA STORAGE</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">
-            <img src={IPModular} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>IP Modular Course</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
+        <div className="card-certs">
+          <ModalImage 
+            className='cert'
+            small={BestPracticesCert}
+            large={BestPracticesCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
 
-        <div className="card">
-          <div className="card-header">
-            <img src={ITandCPECareers} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>CAREERS IN IT AND CPE IN THE IT INDUSTRY</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">
-            <img src={EmbeddedSystems} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>LATEST TECHNOLOGY IN EMBEDDED SYSTEMS</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={DataMiningCert}
+            large={DataMiningCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
 
-        <div className="card">
-          <div className="card-header">
-            <img src={LawsAndProfessional} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>LAWS & PROFESSIONAL CONDUCTS FOR ICT PROFESSIONALS</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">
-            <img src={ReactNative} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>MOBILE APPLICATION UI DEVELOPMENT USING REACT NATIVE</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={DataPrivacyCert}
+            large={DataPrivacyCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
 
-        <div className="card">
-          <div className="card-header">
-            <img src={QATesting} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>QA Testing Conference: QA Tester Share Successful Software Testing Case Studies</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="card-header">
-            <img src={WirelessNetworks} alt="" style={ {width: '100%', height: '250px'} } />
-          </div>
-          <div className="card-body">
-              <h1>WIRELESS NETWORKS & COMMUNICATIONS</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={CloudComputingCert}
+            large={CloudComputingCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
 
-        <div className="card">
-          <div className="card-header">
-            <img src={ArduinoProjects} alt="" style={ {width: '100%', height: '250px'} } />
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={IPModularCert}
+            large={IPModularCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
-          <div className="card-body">
-              <h1>WORKSHOPS ON ARDUINO PROJECTS</h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, blanditiis rerum? Asperiores eaque sint repellat aut, adipisci magnam, eius consequuntur quibusdam dolor, suscipit explicabo voluptatum temporibus itaque. Iusto, esse laborum?</p>
-              <button>View Certificate</button>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={ITandCPECareersCert}
+            large={ITandCPECareersCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={EmbeddedSystemsCert}
+            large={EmbeddedSystemsCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={LawsAndProfessionalCert}
+            large={LawsAndProfessionalCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={MixedAugmentedCert}
+            large={MixedAugmentedCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={ReactNativeCert}
+            large={ReactNativeCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={QATestingCert}
+            large={QATestingCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={WirelessNetworkCert}
+            large={WirelessNetworkCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
+          </div>
+        </div>
+
+        <div className="card-certs">
+          <ModalImage
+            className='cert'
+            small={ArduinoProjectsCert}
+            large={ArduinoProjectsCert}
+            alt="Hello World!"
+          />
+          <div className="card-certs-body">
+            <p>Click Certificate to zoom</p>
           </div>
         </div>
       </div>
